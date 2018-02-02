8d34d775782b9f16273eb38e6ff9b4f7ce83c0ec
t-16x <6717385+t-16x@users.noreply.github.com>
2018-02-01 19:08:12 -0600

    removed ad

--- kali_cleaner.sh
+++ kali_cleaner.sh
@@ -4,11 +4,7 @@ echo -e
 YELLOW="\033[1;33m"
 RED="\033[0;31m"
 ENDCOLOR="\033[0m"
-echo -e $RED"================Kali-Cleaner========================================"$ENDCOLOR
-echo -e $RED"=      Little cleaner for Kali by MasterButcher [:]                ="$ENDCOLOR
-echo -e $RED"===================================================================="$ENDCOLOR
-echo -e
-echo -e
+
                                                                                                                                                                                                                                          
 OLDCONF=$(dpkg -l|grep "^rc"|awk '{print $2}')
 CURKERNEL=$(uname -r|sed 's/-*[a-z]//g'|sed 's/-386//g')
